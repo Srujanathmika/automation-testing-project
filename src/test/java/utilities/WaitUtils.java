f050c1d5f2587b45a30c86325eaae5b5364fa1d0
K. Srujarathmika <srujunakatam@gmail.com>
2025-07-09 22:35:49 +0530

    Made changes to WaitUtils.java

--- src/test/java/utilities/WaitUtils.java
+++ src/test/java/utilities/WaitUtils.java
@@ -1,10 +1,39 @@
 package utilities;
 
 import org.openqa.selenium.*;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class WaitUtils {
+
+    //wait for Element to be loaded
+    public static WebElement waitForElementToBeVisible(WebDriver driver, By locator, int timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    //wait for Element to be clickable
+    public static WebElement waitForElementToClick(WebDriver driver, By locator, int timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    //wait for Element to be alert
+    public static WebElement waitForAlert(WebDriver driver, By locator, int timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    //wait for Element to be alert
+    public static WebElement waitForElementPresence(WebDriver driver, By locator, int timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+    //wait for frame and switch to it
+    public static WebElement waitForFrameAndSwitchToIt(WebDriver driver, By locator, int timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
 
 }
